67721dd2a95670ca297ad4fda538e216b1e84f67
Manish Sahni <Manish-Sahni>
2023-02-14 11:15:35 +0530

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace WpfApp1
+{
+    /// <summary>
+    /// floateraction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Butoon_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float num1 = float.Parse(textBox1.Text);
+                float num2 = float.Parse(textBox2.Text);
+                float result = num1 / num2;
+                label3.Content = result.ToString();
+            }
+            catch (DivideByZeroException)
+            {
+                label3.Content = "Cannot didvide by zero.";
+            }
+            catch (FormatException)
+            {
+                label3.Content = "Please enter only non-decimal number.";
+            }
+            catch (Exception ex)
+            {
+                label3.Content = ex.GetType() + ":" + ex.Message;
+            }
+           
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
